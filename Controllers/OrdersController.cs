e6ba347a0db278d1c524756bc1c5aa330aa4ad3d
victor.kasmin <victor.kasmin@gmail.com>
2018-11-04 09:38:44 +0300

    Step #1

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using Shop.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Shop.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ShopContext _db;
+
+        public OrdersController(ShopContext db)
+        {
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Order> orders = _db.Orders.ToList();
+
+            //var iwm = new IndexViewModel { OrderList = orders, OrderItems = orderItems };
+
+            return View(orders);
+        }
+    }
+}
---
+++ ViewModels/IndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Shop.Models
+{
+    public class IndexViewModel
+    {
+        // public int OrderNumber { get; set; }
+        public List<Order> OrderList { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        // public decimal OrderSum { get; set; }
+    }
+}
